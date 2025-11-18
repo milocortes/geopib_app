d110004972c4c09ed33644eab5214e984a9b2ec0
Hermilo Cortes <hermilocg@hotmail.com>
2025-11-18 17:35:18 -0600

    agrega rutas acceso db

--- packages/ui/src/hero.rs
+++ packages/ui/src/hero.rs
@@ -1,4 +1,5 @@
-use dioxus::{prelude::*};
+use api::save_dog;
+use dioxus::prelude::*;
 
 const HERO_CSS: Asset = asset!("/assets/styling/hero.css");
 //const HEADER_SVG: Asset = asset!("/assets/header.svg");
@@ -6,27 +7,41 @@ const HERO_CSS: Asset = asset!("/assets/styling/hero.css");
 #[component]
 pub fn Hero() -> Element {
 
+    let mut img_src = use_loader(|| async move {
+        dioxus::Ok(
+            reqwest::get("https://dog.ceo/api/breeds/image/random")
+                .await?
+                .json::<serde_json::Value>()
+                .await?["message"]
+                .to_string(),
+        )
+    })?;
 
     rsx! {
         document::Link { rel: "stylesheet", href: HERO_CSS }
-        /* 
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
-        }
-        */
 
         div { style: "text-align: center;",
             h1 { "Esta es la página de inicio de la página" }
         }     
+
+        div { id : "dogview", 
+            img { id : "dogimg", src : "{img_src}"}
+        } 
+
+        div { id : "buttons",
+
+            button {  
+                id : "skip", 
+                onclick : move |_| img_src.restart(),
+                "skip"
+            }
+            button { 
+                id : "save",
+                onclick : move |_| async move { _ = save_dog(img_src()).await}, 
+                "save!!"
+             }
+
+        }
 
     }
 }
